29f1ce40172afc8b5b9c96fdfa812157ef43f792
betterhell <dev.betterhell@gmail.com>
2025-03-07 14:58:58 +0300

    remove styles from css and add to ts

--- src/utils/circle.ts
+++ src/utils/circle.ts
@@ -1,7 +1,7 @@
 import { Rect } from '../models/rect.js';
 import { state } from '../state.js';
 
-export const createCircle = (rect: Rect): void => {
+export const createCircle: (rect: Rect) => void = (rect: Rect): void => {
     state.circle = document.createElement('div');
     state.circle.classList.add('circle');
     document.body.appendChild(state.circle);
@@ -14,16 +14,23 @@ export const createCircle = (rect: Rect): void => {
     state.circle.style.height = `${diameter}px`;
     state.circle.style.left = `${x - diameter / 2}px`;
     state.circle.style.top = `${y - diameter / 2}px`;
+
+    state.circle.style.position = 'fixed';
+    state.circle.style.borderRadius = '50%';
+    state.circle.style.pointerEvents = 'none';
+    state.circle.style.transform = 'scale(0)';
+    state.circle.style.transition = 'transform 1s ease-out';
+
     state.circle.style.backgroundColor = state.newColor;
 };
 
-export const circlePropagation = (): void => {
+export const circlePropagation: () => void = (): void => {
     if (state.circle) {
         state.circle.style.transform = 'scale(2)';
     }
 };
 
-export const circleRemove = (): void => {
+export const circleRemove: () => void = (): void => {
     if (state.circle) {
         state.circle.remove();
         state.circle = null;
